27257d02f5b86d3ebd3a942815bc9443a30e36e2
PRANEETH2422 <praneethwaran03@gmail.com>
2025-07-24 22:52:42 +0530

    Delete pr20.c

--- pr20.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char ch;
-    printf("Enter a character: ");
-    scanf(" %c", &ch);  // space before %c to consume leftover newline
-    printf("ASCII value of '%c' = %d\n", ch, ch);
-    return 0;
-}
